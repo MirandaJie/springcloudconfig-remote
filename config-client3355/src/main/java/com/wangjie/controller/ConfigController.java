467954c0de0454519ed8d885cec7d04e3a1ef7bf
wangjie <miranda9477@163.com>
2020-12-10 04:21:43 +0800

    配置文件手动刷新

--- config-client3355/src/main/java/com/wangjie/controller/ConfigController.java
+++ config-client3355/src/main/java/com/wangjie/controller/ConfigController.java
@@ -1,11 +1,13 @@
 package com.wangjie.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("config")
+@RefreshScope //post请求刷新 http://localhost:3355/refresh
 public class ConfigController {
     @Value("${config.info}")
     private String info;
